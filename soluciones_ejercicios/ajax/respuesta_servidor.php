5116e8f6da9576d33cc4096a66ba6e1376d2dea6
Ismael Ariel <ismael_ariel@hotmail.com>
2013-11-05 21:35:57 -0600

    ejemplos ajax

---
+++ soluciones_ejercicios/ajax/respuesta_servidor.php
@@ -0,0 +1,6 @@
+<h1>Nuevo Texto </h1>
+<p>
+   Este parrafo es totalmente Nuevo, se obtuvo 
+   al hacer una peticion remota al servidor
+   <strong>la hora es: <?php echo date('Y-m-d H:i:s'); ?> </strong> 
+</p>
